d3301b5263546616758efe272b6f701735b3da5a
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-11-30 13:30:52 +0530

    setting up app

--- api/index.py
+++ api/index.py
@@ -4,3 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from datetime import datetime
+
+# App setup
+app = Flask(__name__, static_folder="../static", template_folder="../templates")
+app.secret_key = os.environ.get("SECRET_KEY", "dev-secret")
